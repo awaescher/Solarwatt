bb4931fad4bed634eb8799f180314783b019cbc4
Andreas Wäscher <awaescher@sodacore.net>
2017-11-07 23:27:41 +0100

    Add Microcharts packages

--- Sundays.Client.App/Sundays.Client.App/MainPage.xaml.cs
+++ Sundays.Client.App/Sundays.Client.App/MainPage.xaml.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using SkiaSharp;
+using Microcharts;
 
 namespace Sundays.Client.App
 {
@@ -12,6 +14,42 @@ namespace Sundays.Client.App
 		public MainPage()
 		{
 			InitializeComponent();
+		}
+
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+
+			var entries = new[]
+			{
+				new Microcharts.Entry(200)
+				{
+					Label = "January",
+					ValueLabel = "200",
+					Color = SKColor.Parse("#266489")
+				},
+				new Microcharts.Entry(400)
+				{
+					Label = "February",
+					ValueLabel = "400",
+					Color = SKColor.Parse("#68B9C0")
+				},
+				new Microcharts.Entry(-100)
+					{
+					Label = "March",
+					ValueLabel = "-100",
+					Color = SKColor.Parse("#90D585")
+				}
+			};
+
+			var chart = new BarChart() { Entries = entries };
+			// or: var chart = new PointChart() { Entries = entries };
+			// or: var chart = new LineChart() { Entries = entries };
+			// or: var chart = new DonutChart() { Entries = entries };
+			// or: var chart = new RadialGaugeChart() { Entries = entries };
+			// or: var chart = new RadarChart() { Entries = entries };
+
+			this.chartView.Chart = chart;
 		}
 	}
 }
